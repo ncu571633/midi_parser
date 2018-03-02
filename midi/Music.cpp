2b52077b57ee0155a62d9c2e28cc6b8ded4dc82b
ncu571633 <hechuanjun@msn.com>
2018-03-02 11:46:21 -0500

    modified:   Music.cpp 	modified:   Music.hpp

--- midi/Music.cpp
+++ midi/Music.cpp
@@ -1,8 +1,8 @@
 #include "Music.hpp"
 
-const std::string MusicUtility::pianoKeyBoard[] = {std::string("c")};//{"c","c+d-","d", "d+e-","e","f","f+g-","g","g+a-","a","a+b-","b"};
-int MusicUtility::PianoOctaveKeyNumber = sizeof(MusicUtility::pianoKeyBoard) / sizeof(MusicUtility::pianoKeyBoard[0]);   // 12
-
+const std::string MusicUtility::scale[] = {"c","c+d-","d", "d+e-","e","f","f+g-","g","g+a-","a","a+b-","b"};
+int MusicUtility::scaleSize = sizeof(MusicUtility::scale) / sizeof(MusicUtility::scale[0]);   // 12
+std::vector<int> MusicUtility::NoteArray;
 
 int MusicUtility::code(const std::string& note)
 {
@@ -20,21 +20,23 @@ std::string MusicUtility::decode(int number)
 
 void MusicUtility::initNoteArray()
 {
+    MusicUtility::NoteArray.resize(MusicUtility::NoteArraySize);
+
 	for (int j=0; j<MusicUtility::NoteArraySize; j++)
 	{
         std::string note = MusicUtility::decode(j);
-		for (int i=0; i<MusicUtility::PianoOctaveKeyNumber; i++)
+		for (int i=0; i<MusicUtility::scaleSize; i++)
 		{
 			NoteArray[j] = 0;
-			if (	(pianoKeyBoard[i][0]==note[1]) 
+			if (	(scale[i][0]==note[1]) 
 					&&	(
-							(	(pianoKeyBoard[i][1]=='\0') && (note[2]=='\0')	)
-						||	(	(pianoKeyBoard[i][0]==note[1]) && (pianoKeyBoard[i][1]==note[2]) && (note[2]!='\0') )
-						||	(	(pianoKeyBoard[i][2]==note[1]) && (pianoKeyBoard[i][3]==note[2]) && (note[2]!='\0') )
+							(	(MusicUtility::scale[i][1]=='\0') && (note[2]=='\0')	)
+						||	(	(MusicUtility::scale[i][0]==note[1]) && (MusicUtility::scale[i][1]==note[2]) && (note[2]!='\0') )
+						||	(	(MusicUtility::scale[i][2]==note[1]) && (MusicUtility::scale[i][3]==note[2]) && (note[2]!='\0') )
 						)
 			)
 			{
-				NoteArray[j] = (i + (note[0]-'0')*PIANOOCTAVEKEYNUMBER -8);
+				MusicUtility::NoteArray[j] = (i + (note[0]-'0')*MusicUtility::scaleSize-8);
 				break;
 			}
 		}
@@ -65,17 +67,27 @@ int MusicUtility::Note2KeyNumber(std::string& note)
 std::string MusicUtility::KeyNumber2Note(int PianoKeyNumber)
 {
     std::string ret;
-/*	//because "%", the result belongs to [0, PIANOOCTAVEKEYNUMBER-1]
-	int index = (PianoKeyNumber+9)%PIANOOCTAVEKEYNUMBER-1;
-	n[0] = ((PianoKeyNumber+9) / PIANOOCTAVEKEYNUMBER) + '0';
+/*	//because "%", the result belongs to [0, scaleSize-1]
+	int index = (PianoKeyNumber+9)%scaleSize-1;
+	n[0] = ((PianoKeyNumber+9) / scaleSize) + '0';
 	if ( index < 0 )
 	{
 		n[0]--;
-		index+=PIANOOCTAVEKEYNUMBER;
+		index+=scaleSize;
 	}
-	n[1] = pianoKeyBoard[index][0];
-	n[2] = pianoKeyBoard[index][1];
+	n[1] = scale[index][0];
+	n[2] = scale[index][1];
 	n[3] = '\0';
     */
     return ret;
+}
+
+int MusicUtility::Note2MidiNumber(std::string& note)
+{
+    return MusicUtility::KeyNumber2Midi(MusicUtility::Note2KeyNumber(note));
+}
+
+std::string MusicUtility::MidiNumber2Note(int PianoKeyNumber)
+{
+    return MusicUtility::KeyNumber2Note(MusicUtility::Midi2KeyNumber(PianoKeyNumber));
 }
--- midi/Music.hpp
+++ midi/Music.hpp
@@ -4,13 +4,22 @@
 #include <string>
 #include <vector>
 
+// MusicUtility class does the conversion among the notations below:
+// music note(English notation): pitch-scale: like 4c, 5d
+// piano key number(88 key): 40 (c4) 
+// midi number
+
 class MusicUtility
 {
     private:
-        static const std::string pianoKeyBoard[];
-        static int PianoOctaveKeyNumber;
+        // 12-tone chromatic scale
+        static const std::string scale[];
+        static int scaleSize;
+       
+        // NoteArray:   index: 
+        //              value: note
+        // pre-calculate
         static const int NoteArraySize = 1024;
-        
         static std::vector<int> NoteArray;
         static void initNoteArray();
 
@@ -24,5 +33,9 @@ class MusicUtility
         // note <-> piano key number
         static int Note2KeyNumber(std::string& note);
         static std::string KeyNumber2Note(int PianoKeyNumber);
+
+        // note <-> midi number
+        static int Note2MidiNumber(std::string& note);
+        static std::string MidiNumber2Note(int PianoKeyNumber);
 };
 #endif
